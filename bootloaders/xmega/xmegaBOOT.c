68c6ac221548f01ef103d3654fa74161a072c961
bob frazier <bobf@mrp3.com>
2017-01-02 14:21:55 -0800

    fixed 'startup' problem with flashing images >64k via STK500v2 protocol

--- bootloaders/xmega/xmegaBOOT.c
+++ bootloaders/xmega/xmegaBOOT.c
@@ -151,6 +151,8 @@ void SP_WriteApplicationPage(uint32_t address);
 uint8_t readNVMData(uint8_t cmd, uint16_t iIndex);
 
 
+//#define OLD_BLINK_METHOD /* old way */
+
 /* Use the F_CPU and MAX_TIME_COUNT as defined in Makefile */
 
 /* 20070707: hacked by David A. Mellis - after this many errors give up and launch application */
@@ -305,14 +307,14 @@ void byte_response(uint8_t);
 void nothing_response(void);
 char gethex(void);
 void puthex(char);
-void soft_boot(void);
+void soft_boot(void) __attribute__((noreturn));
 char smart_delay_ms(uint16_t ms);
 
 #ifdef USE_STK500V2
 void flushin(void);
 uint8_t do_getch_and_checksum(void);
 void do_putch_and_checksum(uint8_t chPut);
-#endif USE_STK500V2
+#endif // USE_STK500V2
 
 
 // DEBUG STUFF
@@ -391,7 +393,9 @@ uint8_t buff[PAGE_SIZE + 2]; // was 512 - largest page size 0x200 on XMEGA
 #endif // USE_STK500V2
 
 uint8_t error_count;
-
+#ifndef OLD_BLINK_METHOD
+uint8_t blink_mode;
+#endif // OLD_BLINK_METHOD
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // APP START - different definitions for >64K NVRAM and <=64K
@@ -436,6 +440,7 @@ void (*app_start)(void) = 0x0000; // here it's a function pointer with an addres
 int main(void)
 {
 uint8_t ch, ch2, bod;
+register  uint8_t bootflags;
 uint16_t w1;
 uint8_t firstchar;
 #ifdef USE_STK500V2
@@ -455,9 +460,9 @@ unsigned char *p1;
 
   // this part needs to be done right away
 
-  bod = RST_STATUS & 0x3f; // bits 6 and 7 aren't used according to the manual (note bit 6 is defined as RST_SDRF_bm in nearly every header)
+  bootflags = RST_STATUS & 0x3f; // bits 6 and 7 aren't used according to the manual (note bit 6 is defined as RST_SDRF_bm in nearly every header)
 #ifdef RST_SDRF_bm
-  RST_STATUS = 0x7f; // bit 6 also
+  RST_STATUS = 0x7f; // clear bit 6 also
 #else // RST_SDRF_bm
   RST_STATUS = 0x3f; // write 1 bits to clear everything (so I don't loop)
 #endif // RST_SDRF_bm
@@ -469,14 +474,14 @@ unsigned char *p1;
   CCP = CCP_IOREG_gc;        // 0xd8 - see D manual, sect 3.14.1 (protected I/O)
   WDT_WINCTRL = WDT_WCEN_bm; // sets watchdog 'window' timer "enable" bit to zero - bit 0 must be set to change bit 1 - section 9.7.2
 
-  // the 'bod' value will be non-zero if I did a normal reboot, or had the WDT time out,
-  // or had a brownout, or soft reboot, or hard reboot.  But if I merely jump here, it's
-  // going to be zero, so I'll execute a soft reboot to make sure I'm not boot-looping
+  // the 'bootflags' value will be non-zero if I did a normal reboot, or had the WDT time
+  // out, or had a brownout, or soft reboot, or hard reboot.  But if I merely jump here,
+  // it's going to be zero, so I'll execute a soft reboot to make sure I'm not boot-looping
   // due to jumping into erased NVRAM and executing NOP instructions until I get here.
   // (that generally doesn't work very well since the hardware doesn't properly reset)
 
-  if(!bod) // if 'bod' is zero, I executed this directly.  So to prevent boot-loops after a bad flash, do this
-  {        // this forces a 'soft boot' which re-enters the bootloader normally, preventing a boot-loop
+  if(!bootflags) // if 'bootflags' is zero, I executed this directly.  So to prevent boot-loops after a bad flash,
+  {              // I'll force a 'soft boot' which re-enters the bootloader normally, preventing a boot-loop
     soft_boot();
   }
 
@@ -484,8 +489,8 @@ unsigned char *p1;
   // NOW assign 'ch' to the watchdog bit (3) and bod to the B.O.D. bit (2)
   // TODO:  other flags?  I might want to do a soft boot into a special feature, like bootloader flashing
 
-  ch = bod & RST_WDRF_bm /*_BV(3)*/; // watchdog bit, D manual sect 8.5.1
-  bod &= RST_BORF_bm /*_BV(2)*/; // brown out detector (8.5.1)
+  ch = bootflags & RST_WDRF_bm /*_BV(3)*/; // watchdog bit, D manual sect 8.5.1
+  bod = bootflags & RST_BORF_bm /*_BV(2)*/; // brown out detector (8.5.1)
 
   // TODO:  add other "skip the bootloader flash check" flags to this?  like soft boot?
   //        if I soft boot here, it might be an INTERNAL call to 'soft_boot' so another
@@ -508,12 +513,6 @@ unsigned char *p1;
   CCP = CCP_IOREG_gc; // 0xd8 - see D manual, sect 3.14.1 (protected I/O)
   PMIC_CTRL = 0xc0; // binary 11000000  all 3 int levels disabled, round-robin enabled, IVT starts at 10002H
 
-//#else // old code
-//
-//  asm volatile("nop\n\t");
-//
-//#endif // WATCHDOG_MODS
-
   if(bod)
   {
 //  do something special if brown-out detected, like keeping the clock slow
@@ -526,15 +525,15 @@ unsigned char *p1;
 
   // enable BOTH the 32Mhz and 32.768KHz internal clocks [ignore what else might be set for now]
 
-//  OSC_CTRL |= _BV(2) | _BV(1); // sect 6.10.1 - enable 32.768KHz (2), 32Mhz (1).  2Mhz is _BV(0)
-//                               //               _BV(3) and _BV(4) are for PLL and external - will set to 0 later
+  // sect 6.10.1 - enable 32.768KHz _BV(2), 32Mhz _BV(1).  2Mhz is _BV(0)
+  //               _BV(3) and _BV(4) are for PLL and external - will set to 0 later
 
   OSC_CTRL |= OSC_RC32KEN_bm | OSC_RC32MEN_bm; // enable 32khz osc, 32mhz osc - 'D4' manual sect 6.10.1
 
-  if(!(CLK_LOCK & CLK_LOCK_bm/*1*/)) // clock lock bit NOT set, so I can muck with the clock
+  if(!(CLK_LOCK & CLK_LOCK_bm)) // clock lock bit NOT set, so I can muck with the clock
   {                   // note that in the bootloader this should NEVER happen.  check anyway.
 
-    if((CLK_CTRL & CLK_SCLKSEL_gm/*7*/) != CLK_SCLKSEL_RC32M_gc/*1*/) // it's not already 32 Mhz (D manual 6.9.1)
+    if((CLK_CTRL & CLK_SCLKSEL_gm) != CLK_SCLKSEL_RC32M_gc) // it's not already 32 Mhz (D manual 6.9.1)
     {
       // wait until 32mhz clock is 'stable'
 
@@ -542,7 +541,7 @@ unsigned char *p1;
       {
         // spin on oscillator status bit for 32Mhz oscillator
 
-        if(OSC_STATUS & OSC_RC32MRDY_bm/*_BV(1)*/) // 32Mhz oscillator is 'ready' (6.10.2)
+        if(OSC_STATUS & OSC_RC32MRDY_bm) // 32Mhz oscillator is 'ready' (6.10.2)
         {
           break;
         }
@@ -551,28 +550,29 @@ unsigned char *p1;
       // for now, I can allow the clock to NOT be changed if it's
       // not ready.  This prevents infinite loop inside startup code
 
-      if(!(OSC_STATUS & OSC_RC32MRDY_bm/*_BV(1)*/)) // is my oscillator 'ready' ?
+      if(!(OSC_STATUS & OSC_RC32MRDY_bm)) // is my oscillator 'ready' ?
       {
         goto skip_clock; // exit - don't change anything
       }
 
       // switch to 32Mhz clock using internal source
 
-      CCP = CCP_IOREG_gc;// 0xd8 - see D manual, sect 3.14.1 (protected I/O)
-      CLK_CTRL = CLK_SCLKSEL_RC32M_gc/*1*/; // set the clock to 32Mhz (6.9.1)
+      CCP = CCP_IOREG_gc;              // 0xd8 - see D manual, sect 3.14.1 (protected I/O)
+      CLK_CTRL = CLK_SCLKSEL_RC32M_gc; // set the clock to 32Mhz (6.9.1)
     }
 
     if(CLK_PSCTRL != 0)
     {
-      CCP = CCP_IOREG_gc;// 0xd8 - see D manual, sect 3.14.1 (protected I/O)
-      CLK_PSCTRL = CLK_PSADIV_1_gc | CLK_PSBCDIV_1_1_gc/*0*/; // set the clock divider(s) to 1:1 (6.9.2)
+      CCP = CCP_IOREG_gc;                                // 0xd8 - see D manual, sect 3.14.1 (protected I/O)
+      CLK_PSCTRL = CLK_PSADIV_1_gc | CLK_PSBCDIV_1_1_gc; // set the clock divider(s) to 1:1 (6.9.2)
     }
 
 
     // now that I've changed the clock, disable 2Mhz, PLL, and external clocks
     // 32.768KHz should remain active, but I need to make sure it's stable
-//    OSC_CTRL &= ~(_BV(4) | _BV(3) | _BV(0)); // sect 6.10.1 - disable PLL, external, 2Mhz clocks
-    OSC_CTRL &= ~(OSC_PLLEN_bm | OSC_XOSCEN_bm // | OSC_RC2MEN_bm (don't disable 2M oscillator in bootloader)
+    // sect 6.10.1 - disable PLL, external, 2Mhz clocks
+
+    OSC_CTRL &= ~(OSC_PLLEN_bm | OSC_XOSCEN_bm // exclude OSC_RC2MEN_bm (don't disable 2M oscillator in bootloader)
 #ifdef OSC_RC8MCAL // only present in 'E' series
                   | OSC_RC8MEN_bm
 #endif // OSC_RC8MCAL
@@ -585,21 +585,26 @@ unsigned char *p1;
     {
       for(ch2=255; ch2 > 0; ch2--) // this waits up to 256 times longer than just the outer loop
       {
-        if(OSC_STATUS & OSC_RC32KRDY_bm/*_BV(2)*/) // 32.768KHz oscillator is 'ready' (6.10.2)
+        if(OSC_STATUS & OSC_RC32KRDY_bm) // 32.768KHz oscillator is 'ready' (6.10.2)
         {
           goto done_waiting_for_osc_status;
         }
       }
     }
 
-done_waiting_for_osc_status:
+done_waiting_for_osc_status: // at this point the 32.768khz oscillator should be stable enough for the DFLL
 
     // enable DFLL auto-calibration of the 32Mhz internal oscillator
     // (it uses the reasonably precise 32.768KHz clock to do it)
     // This is the BEST clock strategy to use for 32Mhz.
 
-    OSC_DFLLCTRL = OSC_RC32MCREF_RC32K_gc/*0*/; // sect 6.10.7 - select 32.768KHz osc for everything, basically
-    DFLLRC32M_CTRL = DFLL_ENABLE_bm/*1*/; // set the bit to enable DFLL calibration - section 6.11.1
+#ifdef OSC_RC32MCREF_gm /* if this is present, the enum for OSC_RC32MCREF_enum is also present */
+    OSC_DFLLCTRL = OSC_RC32MCREF_RC32K_gc; // sect 6.10.7 - select 32.768KHz osc for everything, basically
+#else // OSC_RC32MCREF_gm not present
+    OSC_DFLLCTRL = 0; // for header files that do not have the enumeration, this will have to do
+#endif // OSC_RC32MCREF_gm
+
+    DFLLRC32M_CTRL = DFLL_ENABLE_bm; // set the bit to enable DFLL calibration - section 6.11.1
   }
 
   // I'll be using the 1.024khz clock (from the 32.768KHz clock) for the real-time counter
@@ -611,7 +616,7 @@ done_waiting_for_osc_status:
   {
     for(ch2=255; ch2 > 0; ch2--)
     {
-      if(OSC_STATUS & OSC_RC32KRDY_bm/*_BV(2)*/) // 32.768KHz oscillator is 'ready' (6.10.2)
+      if(OSC_STATUS & OSC_RC32KRDY_bm) // 32.768KHz oscillator is 'ready' (6.10.2)
       {
         goto done_waiting_again_osc_status;
       }
@@ -620,7 +625,7 @@ done_waiting_for_osc_status:
 
 done_waiting_again_osc_status:
 
-  if(!(OSC_STATUS & OSC_RC32KRDY_bm/*_BV(2)*/)) // is my oscillator 'ready' ?
+  if(!(OSC_STATUS & OSC_RC32KRDY_bm)) // is my oscillator 'ready' ?
   {
     goto skip_clock; // exit - don't change anything else
   }
@@ -628,14 +633,15 @@ done_waiting_again_osc_status:
 
   // RUN-TIME clock - use internal 1.024 khz source.  32khz needed for this
   // (the run-time clock works pretty well this way, scheduling events, CPU wakeup, etc.)
-  CLK_RTCCTRL = 2; // section 6.9.4
+//  CLK_RTCCTRL = CLK_RTCSRC_RCOSC_gc/*was 2, was wrong*/; // section 6.9.4, 7.9.4 in A[U] manual
+  CLK_RTCCTRL = 2; // anything else and it won't boot up
 
 skip_clock:  // go here if clock cannot be assigned for some reason or is already assigned
 
   // regular 'WATCHDOG_MODS' code starts back up here (NOTE: 'WATCHDOG_MODS' are always active now)
 
   // Check if the WDT was used to reset, in which case we don't bootload and skip straight to the code. w00t.
-  if (ch)   // if it's not an external reset... (in this case, JUST checking for watchdog, later BOD as well?)
+  if(ch)   // if it's not an external reset... (in this case, JUST checking for watchdog, later BOD as well?)
   {
     app_start();  // skip bootloader (NOTE:  function call WAY smaller than goto)
   }
@@ -657,7 +663,7 @@ skip_clock:  // go here if clock cannot be assigned for some reason or is alread
   // TODO:  allow this to be used for the A1 series with SERIAL1 ?
 
 #ifdef SERIAL_PORT_REMAP_REG
-  SERIAL_PORT_REMAP_REG = PORT_USART0_bm/*_BV(4)*/; // see 12.13.13 in 'E' manual
+  SERIAL_PORT_REMAP_REG = PORT_USART0_bm; // see 12.13.13 in 'E' manual
 #endif // SERIAL_PORT_REMAP_REG
 
   // PD6 (GPIO 6) must have input pullup resistor (for serial I/O)
@@ -689,7 +695,7 @@ skip_clock:  // go here if clock cannot be assigned for some reason or is alread
 #endif // REMAP
 
 
-// set the CORRECT baud rate NOW.
+// set the CORRECT baud rate NOW.  normally just use 115k baud
 
 #if BAUD_RATE == 115200
 
@@ -732,7 +738,7 @@ skip_clock:  // go here if clock cannot be assigned for some reason or is alread
 
   // section 19.14.5 - USART mode, parity, bits
   // CMODE 7:6 = 00 [async]  PMODE 5:4 = 00 [none]  SBMODE 3 = 0 [1 bit]   CHSIZE 2:0 = 3 (8-bit)
-  SERIAL_USART_CTRLC = USART_CHSIZE_8BIT_gc/*0x03*/; // SERIAL_8N1 - see HardwareSerial.h
+  SERIAL_USART_CTRLC = USART_CHSIZE_8BIT_gc; // SERIAL_8N1 - see HardwareSerial.h
 
 #ifdef USARTD0_CTRLD // which means we have a CTRLD - E5's do
   SERIAL_USART_CTRLD = 0; // always set to zero (E5 special)
@@ -775,6 +781,16 @@ skip_clock:  // go here if clock cannot be assigned for some reason or is alread
   ch2 = NUM_LED_FLASHES << 1;
 #endif // NUM_LED_FLASHES
 
+  // NOTE:  call to 'getch' turns off the LED
+
+  firstchar = 0; // to make sure we don't start the app by accident
+                 // detect whether the first character is being received or not
+
+  // and before anything else, zero out the error counter
+  error_count = 0;
+
+
+#ifdef OLD_BLINK_METHOD
   while (ch2--) // inlined, may be smaller this way
   {
     // this requires ch2 to be twice the loop count
@@ -792,15 +808,12 @@ skip_clock:  // go here if clock cannot be assigned for some reason or is alread
       break;
     }
   }
+#else // OLD_BLINK_METHOD
 
-  // NOTE:  call to 'getch' turns off the LED
+  blink_mode = ch2; // this causes 'getch' to do the blinking
+  LED_PORT |= LED_PIN_BIT;          // turn on the LED to indicate receiving data
 
-  firstchar = 0; // to make sure we don't start the app by accident
-                 // detect whether the first character is being received or not
-
-  // and before anything else, zero out the error counter
-  error_count = 0;
-
+#endif // OLD_BLINK_METHOD
 
   /* forever loop */
   for (;;)
@@ -1490,12 +1503,10 @@ error_flush_send_reply:
             if (msgBuffer[4] == 0x50)
             {
               answerByte = readNVMData(NVM_CMD_READ_FUSES_gc, 0);
-//                answerByte = boot_lock_fuse_bits_get(GET_LOW_FUSE_BITS);
             }
             else if (msgBuffer[4] == 0x58)
             {
               answerByte = readNVMData(NVM_CMD_READ_FUSES_gc, 1); // low/high fuse byte is wrong; xmega has up to 6 of them
-//                answerByte = boot_lock_fuse_bits_get(GET_HIGH_FUSE_BITS);
             }
             else if (msgBuffer[4] == 0x40) // load memory page low byte
             {
@@ -1639,7 +1650,7 @@ error_flush_send_reply:
         msgLength = 4;
         msgBuffer[1] = STATUS_CMD_OK;
 #ifdef LOCKBIT_LOCKBITS
-        msgBuffer[2] = LOCKBIT_LOCKBITS; // boot_lock_fuse_bits_get( GET_LOCK_BITS );
+        msgBuffer[2] = LOCKBIT_LOCKBITS;
 #else // no LOCKBIT_LOCKBITS
         msgBuffer[2] = NVM_LOCKBITS; // this is how it's defined in the A1 files
 #endif // LOCKBIT_LOCKBITS
@@ -1801,7 +1812,6 @@ error_flush_send_reply:
           // Read FLASH
           do
           {
-//#if defined(RAMPZ)
 #ifdef RAMPZ // (FLASHEND > 0x10000)
             if(tempaddress >= BOOT_SECTION_START)
             {
@@ -1811,7 +1821,7 @@ error_flush_send_reply:
             {
               data = pgm_read_word_far(tempaddress);
             }
-#else // FLASHEND > 0x10000
+#else // !RAMPZ
             if(address >= BOOT_SECTION_START)
             {
               data = 0; // assume boot section is all 0's for the sake of flash verification
@@ -1820,7 +1830,7 @@ error_flush_send_reply:
             {
               data = pgm_read_word_near(tempaddress);
             }
-#endif // FLASHEND > 0x10000
+#endif // RAMPZ or not
             *(p1++) = (unsigned char)data;    //LSB
             *(p1++) = (unsigned char)(data >> 8);  //MSB
 
@@ -1963,10 +1973,16 @@ char smart_delay_ms(uint16_t ms)
 // 'getch()' and 'putch()' - basic serial I/O functions
 char getch(void)
 {
-  uint32_t count = 0;
-  register char rVal;
+static uint32_t count = 0;
+register char rVal;
 
-  LED_PORT &= ~LED_PIN_BIT;          // turn off the LED to indicate receiving data
+#ifndef OLD_BLINK_METHOD
+  if(!blink_mode)
+#endif // OLD_BLINK_METHOD
+  {
+    LED_PORT &= ~LED_PIN_BIT;          // turn off the LED to indicate receiving data
+    count = 0;
+  }
 
   while(!((SERIAL_USART_STATUS) & USART_RXCIF_bm)) // wait for RX data
   {
@@ -1974,7 +1990,28 @@ char getch(void)
     /* HACKME:: here is a good place to count times*/
     count++;
 
-    if (count > (uint32_t)(MAX_TIME_COUNT)) // delay period for firmware flashing
+#ifndef OLD_BLINK_METHOD
+    if(blink_mode > 0)
+    {
+      if(count > (uint32_t)(F_CPU / 160))
+      {
+        blink_mode--;
+
+        if(!blink_mode || (blink_mode & 1))
+        {
+          LED_PORT &= ~LED_PIN_BIT;          // turn off the LED to indicate receiving data
+        }
+        else
+        {
+          LED_PORT |= LED_PIN_BIT;          // turn on the LED to indicate receiving data
+        }
+
+        count = 0;
+      }
+    }
+    else
+#endif // OLD_BLINK_METHOD
+      if (count > (uint32_t)(MAX_TIME_COUNT)) // delay period for firmware flashing
     {
       app_start();
       soft_boot();
@@ -1984,7 +2021,12 @@ char getch(void)
   SERIAL_USART_STATUS = USART_RXCIF_bm; // forcibly clear the bit
   rVal = SERIAL_USART_DATA;
 
-  LED_PORT |= LED_PIN_BIT;          // turn on the LED to indicate receiving data
+#ifndef OLD_BLINK_METHOD
+  if(!blink_mode)
+#endif // OLD_BLINK_METHOD
+  {
+    LED_PORT |= LED_PIN_BIT;          // turn on the LED to indicate receiving data
+  }
 
   return rVal;
 }
